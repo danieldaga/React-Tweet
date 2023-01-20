9a9b86b153659f9299637de3a61da2426924eb3d
danieldaga <daniel@arhrestauracion.com>
2023-01-20 20:36:49 +0100

    iteration 1 done

---
+++ src/components/Tweet.js
@@ -0,0 +1,38 @@
+function Tweet({name, image, handle, timestamp, message}) {
+  return (
+    <div className="tweet">
+      <img
+        src={image}
+        className="profile"
+        alt="profile"
+      />
+
+      <div className="body">
+        <div className="top">
+          <span className="user">
+            <span className="name">{name}</span>
+            <span className="handle">@{handle}</span>
+          </span>
+
+          <span className="timestamp">{timestamp}</span>
+        </div>
+
+        <p className="message">
+          {message}
+        </p>
+
+        <div className="actions">
+          {/* Font Awesome icons */}
+          <i className="far fa-comment"></i>
+          <i className="fas fa-retweet"></i>
+          <i className="far fa-heart"></i>
+          <i className="fas fa-share"></i>
+        </div>
+      </div>
+
+      <i className="fas fa-ellipsis-h"></i>
+    </div>
+  );
+}
+
+export default Tweet;
